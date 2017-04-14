411b63621a301c28ad49a8e94d16937875c2f52c
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2017-04-14 17:29:28 +0200

    implemented writeable

---
+++ test/writable/synchronous.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const {PromisedWritable} = require("../../");
+const {DataStream} = require("scramjet");
+const assert = require('assert');
+
+const data = [1,2,3,4,5];
+
+// synchronous readable
+module.exports =
+    () => new Promise((res, rej) => {
+        const ret = [];
+        DataStream.fromArray(data)
+            .pipe(new PromisedWritable({
+                objectMode: true,
+                write({chunk}) {
+                    ret.push(chunk);
+                }
+            }))
+            .on("error", (err) => rej(err))
+            .on("end", () => res(ret));
+    })
+    .then(
+        (arr) => {
+            console.log(arr);
+            assert.equal(arr.reduce((a, i) => a + i, 0), 15);
+        }
+    );
